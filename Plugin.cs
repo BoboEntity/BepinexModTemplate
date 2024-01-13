dcb0ec7a88030b0b19685911f5684d70b11d20a6
Owner <Owner@DESKTOP-8IOSCVJ>
2024-01-13 13:35:54 -0600

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using Utilla;
+
+namespace ModTemplate
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.11")]
+    [BepInPlugin("com.Author.ModName", "ModName", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // code
+        }
+
+        private void Start()
+        {
+            Events.GameInitialized += OnGameInitialized;
+        }
+
+        // when the game starts up
+        private void OnGameInitialized(object sender, EventArgs e)
+        {
+            // code
+        }
+
+        // What happens every frame 
+        private void Update()
+        {
+            // code
+        }
+
+        private void FixedUpdate()
+        {
+            // code
+        }
+
+        // If you want a GUI
+        private void OnGUI()
+        {
+            // code
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModTemplate
+{
+    internal class PluginInfo
+	{
+		public const string GUID = "com.Author.ModName";
+
+		public const string Name = "ModName";
+
+		public const string Version = "1.0.0";
+	}
+}
